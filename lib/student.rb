05061ce5b3a86d9666517cc7f4610cac8645e8d9
Eric Storey <estorey@gmail.com>
2019-04-14 20:17:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
-attr_accessor :name, :grade
-attr_reader :id
- 
+  attr_accessor :name, :grade
+  attr_reader :id
+   
   def initialize(id=nil, name, grade)
-    @id = id
-    @name = name
-    @grade = grade
+      @id = id
+      @name = name
+      @grade = grade
   end
 
 end
